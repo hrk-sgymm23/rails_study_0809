e258047abff49048b8401ee1dc85ceec32edb215
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2022-08-23 23:59:32 +0900

    ユーザページ実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     resources :favorites, only:[:create, :destroy]
   end
 
-  resources :users, only:[:index, :show]
-  
+  resources :users, only:[:index, :show, :edit, :update]
+
 end
