81082725efe0c60c72621bb703226fdffdd521fd
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2022-08-23 23:25:44 +0900

    ユーザ一覧まで

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,8 @@
 class Post < ApplicationRecord
 
     attachment :travel_image
-
     belongs_to :user
     has_many :comments, dependent: :destroy
-
     has_many :favorites, dependent: :destroy
 
     def favorited_by?(user)
